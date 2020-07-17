1c662cefd0e2b5b88b606424adac7175886feb0b
Ben Spencer <dangerous.ben@gmail.com>
2020-07-17 12:22:26 +0100

    Typeclass instances for Reader.
    - Alternative
    - Defer
    - Monad
    - Monoid
    - Eq

--- util/src/test/scala/io/catbird/util/arbitrary.scala
+++ util/src/test/scala/io/catbird/util/arbitrary.scala
@@ -2,8 +2,9 @@ package io.catbird.util
 
 import com.twitter.concurrent.AsyncStream
 import com.twitter.conversions.DurationOps._
+import com.twitter.io.Reader
 import com.twitter.util.{ Future, Return, Try, Var }
-import org.scalacheck.{ Arbitrary, Cogen }
+import org.scalacheck.{ Arbitrary, Gen, Cogen }
 
 trait ArbitraryInstances {
   implicit def futureArbitrary[A](implicit A: Arbitrary[A]): Arbitrary[Future[A]] =
@@ -17,6 +18,20 @@ trait ArbitraryInstances {
 
   implicit def asyncStreamArbitrary[A](implicit A: Arbitrary[A]): Arbitrary[AsyncStream[A]] =
     Arbitrary(A.arbitrary.map(AsyncStream.of))
+
+  // Note that this doesn't cover BufReader or InputStreamReader currently
+  private def readerGen[A: Arbitrary]: Gen[Reader[A]] =
+    Gen.frequency(
+      1 -> Reader.empty[A],  // empty reader defined inline
+      3 -> Arbitrary.arbitrary[A].flatMap(Reader.value(_)),  // FutureReader (used for both fromFuture and value)
+      1 -> Reader.exception(new Exception),  // also FutureReader but making sure we exercise the failed case
+      3 -> Gen.listOf(Arbitrary.arbitrary[A]).flatMap(Reader.fromSeq(_)),  // IteratorReader
+      2 -> Arbitrary.arbitrary[AsyncStream[A]].flatMap(Reader.fromAsyncStream(_)),  // uses Pipe
+      2 -> readerGen[A].map(Reader.value(_).flatten)  // flatten any of the other types
+    )
+
+  implicit def readerArbitrary[A: Arbitrary]: Arbitrary[Reader[A]] =
+    Arbitrary(readerGen[A])
 
   implicit def rerunnableArbitrary[A](implicit A: Arbitrary[A]): Arbitrary[Rerunnable[A]] =
     Arbitrary(futureArbitrary[A].arbitrary.map(Rerunnable.fromFuture[A](_)))
